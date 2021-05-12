52514abd937e7389de74e670e029708fe45b530b
Volian0 <40314913+Volian0@users.noreply.github.com>
2021-05-12 16:41:41 +0200

    Added support for extracting assets as temporary files

--- src/Path.cpp
+++ src/Path.cpp
@@ -2,6 +2,8 @@
 
 #include <SDL/SDL.h>
 
+#include <filesystem>
+
 std::string Path::res(const std::string& filename, const std::string& folder)
 {
 	std::string path = folder + "/" + filename;
@@ -23,4 +25,46 @@ std::string Path::user(const std::string& filename, const std::string& folder)
 {
 	static std::string user_path = get_pref_path();
 	return user_path + folder + "/" + filename;
+}
+
+ExtractedRes::ExtractedRes(const std::string& filename, const std::string& folder)
+{
+	std::filesystem::create_directories(Path::user({}, "extracted/" + folder));
+	++_count;
+	_path = Path::user(filename, "extracted/" + folder);
+	auto ifile = SDL_RWFromFile(Path::res(filename, folder).c_str(), "rb");
+	auto ofile = SDL_RWFromFile(_path.c_str(), "wb");
+	auto size = SDL_RWsize(ifile);
+	while (true)
+	{
+		char buffer[512];
+		auto read_n = SDL_RWread(ifile, buffer, 1, sizeof(buffer));
+		if (read_n == 0)
+		{
+			break;
+		}
+		if (SDL_RWwrite(ofile, buffer, 1, read_n) != read_n)
+		{
+			abort();
+		}
+	}
+	SDL_RWclose(ifile);
+	SDL_RWclose(ofile);
+}
+
+ExtractedRes::~ExtractedRes()
+{
+	std::filesystem::remove(_path);
+	--_count;
+	if (_count == 0)
+	{
+		std::filesystem::remove_all(Path::user({}, "extracted"));
+	}
+}
+
+uint32_t ExtractedRes::_count = 0;
+
+const std::string& ExtractedRes::get_path() const
+{
+	return _path;
 }
--- src/Path.h
+++ src/Path.h
@@ -9,3 +9,16 @@ namespace Path
 	std::string user(const std::string& filename, const std::string& folder = {});
 	std::string res(const std::string& filename, const std::string& folder = {});
 };
+
+class ExtractedRes : Unique
+{
+public:
+	ExtractedRes(const std::string& filename, const std::string& folder = {});
+	~ExtractedRes();
+
+	const std::string& get_path() const;
+
+private:
+	std::string _path;
+	static uint32_t _count;
+};
